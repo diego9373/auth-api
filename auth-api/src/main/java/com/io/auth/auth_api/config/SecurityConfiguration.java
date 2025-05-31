4b1f53f013e42cb46f3a547cff64b5fb878cbdf0
DIEGO ALMEIDA <almeidadieo488@gmail.com>
2025-05-31 11:50:09 -0300

    Configurando a classe SecurityConfiguration , methodo reference e authorize

--- auth-api/src/main/java/com/io/auth/auth_api/config/SecurityConfiguration.java
+++ auth-api/src/main/java/com/io/auth/auth_api/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.io.auth.auth_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +16,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(crsf -> crsf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                ).build();
     }
 
 }
